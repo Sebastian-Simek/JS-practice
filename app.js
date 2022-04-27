4a8d527d31a3a170cbf3adb6a8d46b6357851462
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-27 11:26:00 -0700

    Added picked class to cheese

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const sodaBottle = document.getElementById('soda');
-
+const pieceOfCheese = document.getElementById('cheese');
 
 sodaBottle.addEventListener('click', () => {
     sodaBottle.classList.toggle('picked');
+});
+
+pieceOfCheese.addEventListener('click', () => {
+    pieceOfCheese.classList.toggle('picked');
 });
